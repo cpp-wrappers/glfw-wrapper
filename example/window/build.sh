d1cf92723ba2e81fe4927ce52782b98611477c62
fewizz <hazeevaidar@gmail.com>
2020-06-09 05:51:15 -0700

    remove example, todo

--- example/window/build.sh
+++
@@ -1,6 +0,0 @@
-cd ..
-bash prepare_glfw.sh
-cd window
-mkdir build -p
-cd build
-clang++ -ggdb --std=c++17 -o window -I../../../include -I../../../glfw/include ../main.cpp ../../../glfw/build/src/libglfw.so
